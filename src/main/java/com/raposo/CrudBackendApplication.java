465726609cbc7e12a741520c27066713fa1b542b
vhraposo <thevhup@gmail.com>
2023-05-26 03:13:29 -0300

    create maven project

---
+++ src/main/java/com/raposo/CrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.raposo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raposo/CrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raposo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

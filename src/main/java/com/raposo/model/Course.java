1be8ef0462364858b2448c378a830cba98a8cef6
vhraposo <thevhup@gmail.com>
2023-05-27 20:40:09 -0300

    config h2 and send data to angular

--- src/main/java/com/raposo/model/Course.java
+++ src/main/java/com/raposo/model/Course.java
@@ -1,5 +1,7 @@
 package com.raposo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,6 +16,7 @@ public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)

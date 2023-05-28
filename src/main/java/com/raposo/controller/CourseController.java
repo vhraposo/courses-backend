f5198cd6ce38f312bf668208601f6b657213d8d4
vhraposo <thevhup@gmail.com>
2023-05-28 05:32:18 -0300

    add post mapping method

--- src/main/java/com/raposo/controller/CourseController.java
+++ src/main/java/com/raposo/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.raposo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raposo.model.Course;
@@ -24,5 +29,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+       return courseRepository.save(course);
     }
 }

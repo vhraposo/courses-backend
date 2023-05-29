2b271d5f019238e8004f8308d1412421af744f03
vhraposo <thevhup@gmail.com>
2023-05-29 01:47:56 -0300

    add findbyid method

--- src/main/java/com/raposo/controller/CourseController.java
+++ src/main/java/com/raposo/controller/CourseController.java
@@ -2,9 +2,11 @@ package com.raposo.controller;
 
 import java.util.List;
 
+import org.apache.catalina.mapper.Mapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +23,18 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
 
-   
     private final CourseRepository courseRepository;
-   
-
 
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity <Course> findById(@PathVariable Long id){
+       return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
